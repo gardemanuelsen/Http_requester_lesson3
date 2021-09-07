b957b2ba3b0b058d5830ed0c28b1a356c656586f
Carlzon50 <72607945+Carlzon50@users.noreply.github.com>
2021-09-07 14:47:19 +0200

    http client method

---
+++ src/main/java/HttpClient.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class HttpClient {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("httpbin.org", 80);
+
+        socket.getOutputStream().write(
+                ("Get /html HTTP/1.1\r\n" +
+                        "Host: httbin.org\r\n" +
+                        "\r\n").getBytes()
+        );
+
+        InputStream in = socket.getInputStream();
+
+        int c;
+        while ((c = in.read()) != -1) {
+            System.out.print((char) c);
+        }
+    }
+
+
+
+
+}
