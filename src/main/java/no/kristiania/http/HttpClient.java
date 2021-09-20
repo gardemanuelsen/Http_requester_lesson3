093456a8c297e6deb35b0d60a9702e39a47bb6b2
gardemanuelsen <garema@student.kristiania.no>
2021-09-20 18:32:01 +0200

    push it

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -6,8 +6,7 @@ import java.net.Socket;
 
 public class HttpClient {
 
-    public HttpClient(String s, int i, String s1) {
-
+    public HttpClient(String host, int port, String requestTarget) {
     }
 
     public static void main(String[] args) throws IOException {
@@ -21,12 +20,33 @@ public class HttpClient {
         socket.getOutputStream().write(request.getBytes());
 
 
+
+
+
+    }
+
+    private String readCharacters(Socket socket, int contentLength) throws IOException {
+        StringBuilder result = new StringBuilder();
+        InputStream in = socket.getInputStream();
+
+        for (int i = 0; i < contentLength; i++) {
+            result.append((char) in.read());
+        }
+
+        return result.toString();
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder result = new StringBuilder();
         InputStream in = socket.getInputStream();
 
         int c;
-        while ((c = in.read()) != -1) {
-            System.out.print((char) c);
+        while ((c = in.read()) != -1 && c != '\r') {
+            result.append((char)c);
         }
+        //noinspection ResultOfMethodCallIgnored
+        in.read();
+        return result.toString();
     }
 
     public int getStatusCode () {
