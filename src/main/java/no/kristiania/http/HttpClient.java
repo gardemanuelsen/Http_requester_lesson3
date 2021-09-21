6b19867082b2a6bd238fb729cf716a699dbcd384
gardemanuelsen <garema@student.kristiania.no>
2021-09-21 13:25:14 +0200

    4 tests working baby

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -3,45 +3,62 @@ package no.kristiania.http;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.HashMap;
+
+
 
 public class HttpClient {
 
-    public HttpClient(String host, int port, String requestTarget) {
-    }
+    private final int statusCode;
+    private final HashMap<String, String> headerFields = new HashMap<>();
 
-    public static void main(String[] args) throws IOException {
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
 
-        Socket socket = new Socket("httpbin.org", 80);
-
-        String request = "GET /html HTTP/1.1\r\n" +
-                "Host: httbin.org\r\n" +
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n"+
+                "Host: " + host +  "\r\n"+
+                "Connection: close\r\n"+
                 "\r\n";
-
-        socket.getOutputStream().write(request.getBytes());
-
+                socket.getOutputStream().write(request.getBytes());
 
 
+        String statusLine = readLine(socket);
+        this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
 
+        String headerField;
+
+        while(!(headerField = readLine(socket)).isBlank()){
+            int colonPos = headerField.indexOf(':');
+            headerFields.put(headerField.substring(0,colonPos), headerField.substring(colonPos+1).trim());
+        }
 
     }
-
 
     private String readLine(Socket socket) throws IOException {
-        StringBuilder result = new StringBuilder();
+        StringBuilder buffer = new StringBuilder();
         InputStream in = socket.getInputStream();
 
         int c;
-        while ((c = in.read()) != -1 && c != '\r') {
-            result.append((char)c);
+        while((c = in.read()) != -1 && c != '\r') {
+            buffer.append((char) c);
         }
-        //noinspection ResultOfMethodCallIgnored
-        in.read();
-        return result.toString();
+            int expectedNewLine = socket.getInputStream().read();
+            assert expectedNewLine == '\n';
+            return buffer.toString();
     }
+
+
 
     public int getStatusCode () {
-        return 401;
+        return statusCode;
     }
 
+    public String getHeader(String headerName){
+        return headerFields.get(headerName);
+    }
+
+    public int getContentLength() {
+        return Integer.parseInt(getHeader("Content-Length"));
+    }
 }
 
--- src/test/java/HttpTest.java
+++ src/test/java/HttpTest.java
@@ -1,20 +1,33 @@
 import no.kristiania.http.HttpClient;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpTest {
 
     @Test
-    void shouldGetSuccessfulResponseCode() {
-        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
-        assertEquals(200, client.getStatusCode());
+    void shouldReturnStatusCode() throws IOException {
+        assertEquals(200, new HttpClient("httpbin.org",80, "/html").getStatusCode());
     }
 
     @Test
-    void shouldGetFailureResponseCode() {
-        HttpClient client = new HttpClient("httpbin.org", 80, "/status/403");
-        assertEquals(401, client.getStatusCode());
+    void shouldReturnStatusCode404() throws IOException {
+        assertEquals(404, new HttpClient("httpbin.org",80, "/no-such-page").getStatusCode());
+    }
+
+
+    @Test
+    void shouldReturnHeaders() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertEquals("text/html; charset=utf-8",client.getHeader("Content-Type"));
+    }
+
+    @Test
+    void shouldReadContentLength() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertEquals(3741,client.getContentLength());
     }
 
 }
